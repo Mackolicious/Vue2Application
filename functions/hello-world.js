7da00d7d01eae3b95f1062eaa9bfba77dd0e5761
Lennox M <lennox.mcintosh@valtech.com>
2022-10-26 20:43:53 +0100

    Changed verb to GET for function

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
-// Reacts to POST /hello-world
+// Reacts to GET /hello-world
 // eslint-disable-next-line require-await, @typescript-eslint/no-unused-vars
-export async function onRequestPost ({ request }) {
+export async function onRequestGet ({ request }) {
   // ...
   return new Response('Hello world')
 }
